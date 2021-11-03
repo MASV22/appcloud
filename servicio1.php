ea37a315cce8eff189e3c64009065eae97ceb768
MASV22 <miguea74@gmail.com>
2021-11-02 19:54:40 -0500

    main

---
+++ servicio1.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$client = new MongoDB\Client(
+    'mongodb+srv://miguel:admin123456@cluster0.amgor.mongodb.net/MultimediaS?retryWrites=true&w=majority');
+
+$tb=$client->MultimediaS->puntaje;
+$registro=array(
+    "iduser"=>"1458",
+    "fecha"=>"1/1/2023",
+    "puntaje"=>"321654"
+);
+
+$resultado=$tb->insertOne($registro);
+echo $resultado ->getInsertedCount();
+
+
+?>
